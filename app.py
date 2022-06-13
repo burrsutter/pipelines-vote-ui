962b53578ca35e27b28781f847a642941eb7c06f
Burr Sutter <burrsutter@gmail.com>
2022-06-13 18:51:40 +0200

    &

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def stuff():
     rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
     response = requests.get(url=rest_endpoint + "/stuff")
     print(response.content)
-    return "Python " + platform.python_version() + " AND " + response.content.decode().strip('"')
+    return "Python " + platform.python_version() + " & " + response.content.decode().strip('"')
 
 
 @app.route('/templates/<path:path>')
